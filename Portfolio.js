b6ec7f26fe341c09fb9931db4a40c29a5efcd632
Srushti Suresh Salunkhe <157711333+srushtisalunkhe@users.noreply.github.com>
2024-11-01 21:48:39 +0530

    Add Portfolio.js file

---
+++ Portfolio.js
@@ -0,0 +1,7 @@
+var typed = new Typed(".text", {
+     strings:["Frontend Developer...","Web Developer...","B.tech Student..."],
+     typespeed: 100,
+     backspeed: 100,
+     backDely: 1000,
+     loop: true
+});
