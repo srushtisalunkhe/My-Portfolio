e64cdcce09548b4eb9746e8eb39c97a88c6f16ac
Srushti Suresh Salunkhe <157711333+srushtisalunkhe@users.noreply.github.com>
2026-01-03 13:36:43 +0530

    Update Portfolio.js

--- Portfolio.js
+++ Portfolio.js
@@ -1,7 +1,12 @@
 var typed = new Typed(".text", {
-     strings:["Frontend Developer...","Web Developer...","B.tech Student..."],
+     strings: [
+        "Full Stack Python Developer Intern...",
+        "IT Graduate...",
+        "Tech Enthusiast..."
+    ],
      typespeed: 100,
      backspeed: 100,
      backDelay: 1000,
      loop: true
+
 });
